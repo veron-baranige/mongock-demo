a30ad9b49bc5e897ca0b78f5e6248dd886156d59
veronb <veron.b@hsenidoutsourcing.com>
2024-06-03 15:12:20 +0530

    Initial commit

---
+++ src/main/java/dev/veronb/mongock_demo/MongockDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.veronb.mongock_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongockDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongockDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/veronb/mongock_demo/MongockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.veronb.mongock_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongockDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
