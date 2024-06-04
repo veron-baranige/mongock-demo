467b84518c6213bbf70f261123e63b139d939e5a
veronb <veron.b@hsenidoutsourcing.com>
2024-06-04 12:40:22 +0530

    Add sample user controller

---
+++ src/main/java/dev/veronb/mongock_demo/controller/UserController.java
@@ -0,0 +1,23 @@
+package dev.veronb.mongock_demo.controller;
+
+import dev.veronb.mongock_demo.entity.user.User;
+import dev.veronb.mongock_demo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+}
