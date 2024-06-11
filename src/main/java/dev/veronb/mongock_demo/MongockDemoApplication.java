82526a53455c664c2553d6495a435f3200b796da
veronb <veron.b@hsenidoutsourcing.com>
2024-06-11 22:36:35 +0530

    Remove annotation for application.properties use

--- src/main/java/dev/veronb/mongock_demo/MongockDemoApplication.java
+++ src/main/java/dev/veronb/mongock_demo/MongockDemoApplication.java
@@ -1,13 +1,9 @@
 package dev.veronb.mongock_demo;
 
-import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableMongock
-@EnableTransactionManagement
 public class MongockDemoApplication {
 
 	public static void main(String[] args) {
