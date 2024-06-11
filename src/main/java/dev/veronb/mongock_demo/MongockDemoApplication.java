f50040b09d70e00be680779370be50f6edfa03fd
veronb <veron.b@hsenidoutsourcing.com>
2024-06-11 21:48:12 +0530

    Enable transactions

--- src/main/java/dev/veronb/mongock_demo/MongockDemoApplication.java
+++ src/main/java/dev/veronb/mongock_demo/MongockDemoApplication.java
@@ -3,9 +3,11 @@ package dev.veronb.mongock_demo;
 import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableMongock
+@EnableTransactionManagement
 public class MongockDemoApplication {
 
 	public static void main(String[] args) {
