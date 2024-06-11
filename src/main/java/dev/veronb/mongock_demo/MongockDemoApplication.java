22b6694092e2df99dd80596cc29d1938d43f64e5
veronb <veron.b@hsenidoutsourcing.com>
2024-06-11 22:40:47 +0530

    Enable mongock

--- src/main/java/dev/veronb/mongock_demo/MongockDemoApplication.java
+++ src/main/java/dev/veronb/mongock_demo/MongockDemoApplication.java
@@ -1,9 +1,11 @@
 package dev.veronb.mongock_demo;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMongock
 public class MongockDemoApplication {
 
 	public static void main(String[] args) {
