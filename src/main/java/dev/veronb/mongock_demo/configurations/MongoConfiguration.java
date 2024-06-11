c0843c464dbf920c9e047321cd80af544043b382
veronb <veron.b@hsenidoutsourcing.com>
2024-06-11 21:48:40 +0530

    Add mongo transaction manager

---
+++ src/main/java/dev/veronb/mongock_demo/configurations/MongoConfiguration.java
@@ -0,0 +1,24 @@
+package dev.veronb.mongock_demo.configurations;
+
+import com.mongodb.ReadConcern;
+import com.mongodb.ReadPreference;
+import com.mongodb.TransactionOptions;
+import com.mongodb.WriteConcern;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfiguration {
+
+    @Bean
+    public MongoTransactionManager transactionManager(MongoTemplate mongoTemplate) {
+        TransactionOptions transactionalOptions = TransactionOptions.builder()
+                .readConcern(ReadConcern.MAJORITY)
+                .readPreference(ReadPreference.primary())
+                .writeConcern(WriteConcern.MAJORITY.withJournal(true))
+                .build();
+        return new MongoTransactionManager(mongoTemplate.getMongoDatabaseFactory(), transactionalOptions);
+    }
+}
